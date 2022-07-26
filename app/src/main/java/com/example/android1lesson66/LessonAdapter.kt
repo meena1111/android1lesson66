32cb4dd6871b9d8bbd273663104eedcfba058aa7
meena1111 <thia4939@gmail.com>
2022-07-26 00:37:26 -0930

    Initial commit

---
+++ app/src/main/java/com/example/android1lesson66/LessonAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.android1lesson66
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android1lesson66.R
+import com.example.android1lesson66.LessonChar
+
+class LessonAdapter (var context: Context, var arrayList: ArrayList<LessonChar>):
+    RecyclerView.Adapter<LessonAdapter.ItemHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder{
+
+        val itemHolder = LayoutInflater.from(parent.context).inflate(R.layout.item_lesson,parent,false)
+        return ItemHolder(itemHolder)
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        var lessonChar:LessonChar = arrayList.get(position)
+
+        holder.icons.setImageResource(lessonChar.iconsChar!!)
+        holder.lessons.text = lessonChar.lessonChar
+
+        holder.lessons.setOnClickListener{
+            Toast.makeText(context,lessonChar.lessonChar, Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+    class ItemHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var icons = itemView.findViewById<ImageView>(R.id.icons_image)
+        var lessons = itemView.findViewById<TextView>(R.id.lessonTv)
+    }
+}
---
+++ app/src/main/java/com/example/android1lesson66/LessonChar.kt
@@ -0,0 +1,12 @@
+package com.example.android1lesson66
+
+class LessonChar {
+
+    var iconsChar:Int ? = 0
+    var lessonChar:String ? = null
+
+    constructor(iconsChar: Int?, lessonChar: String?) {
+        this.iconsChar = iconsChar
+        this.lessonChar = lessonChar
+    }
+}
